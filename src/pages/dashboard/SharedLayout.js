b5039d296c692e52d5e1c7da7c687c8ea7b36b5c
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-06-14 16:23:37 +0700

    add navbar and logout function

---
+++ src/pages/dashboard/SharedLayout.js
@@ -0,0 +1,46 @@
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { Navbar, SmallSidebar, BigSidebar } from '../../components';
+
+const SharedLayout = () => {
+  return (
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+
+  .dashboard-page {
+    width: 90vw;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+
+  @media only screen and (min-width: 992px) {
+    .dashboard {
+      grid-template-columns: auto 1fr;
+    }
+
+    .dashboard-page {
+      width: 90%;
+    }
+  }
+`;
+
+export default SharedLayout;
